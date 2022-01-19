40923adafcf0eb42dadffbf6a3b2220a9999d869
anthony-ohannessian <anthony.ohannessian@laplateforme.io>
2022-01-19 14:55:45 +0100

    forms

---
+++ index.php
@@ -0,0 +1,101 @@
+<section class= "formulaire">
+
+    <h2 class= "sous-titre" >
+        Connexion
+    </h2>
+    
+    <form class= "form"  method= "post">
+                
+        <div class= "form-group">
+            <input type= "text" name= "login" placeholder= "login" autocomplete= "off">
+        </div>
+
+        <div class= "form-group">
+            <input type= "password" name= "password" placeholder= "password" autocomplete= "off">
+        </div>
+        
+        <div class="form-group">
+            <button type="submit" name= "valider" class="btn ">Valider</button>
+        </div>
+
+        <div class="form-group">
+            <button type="submit" name= "logout" class="btn ">Deco</button>
+        </div>
+
+        <div class="form-group">
+            <button type="submit" name= "delete" class="btn ">Delete</button>
+        </div>
+    </form>
+</section>
+
+<section class= "formulaireIns">
+
+    <h2 class= "sous-titre" >
+        Inscription
+    </h2>
+
+    <form class= "formIns"  method= "post">
+                
+        <div class= "form-group">
+            <input type= "text" name= "login" placeholder= "login" autocomplete= "off">
+        </div>
+
+        <div class= "form-group">
+            <input type= "password" name= "password" placeholder= "password" autocomplete= "off">
+        </div>
+
+        <div class= "form-group">
+            <input type= "text" name= "email" placeholder= "email" autocomplete= "off">
+        </div>
+
+        <div class= "form-group">
+            <input type= "text" name= "firstname" placeholder= "firstname" autocomplete= "off">
+        </div>
+
+        <div class= "form-group">
+            <input type= "text" name= "lastname" placeholder= "lastname" autocomplete= "off">
+        </div>
+
+        <div class="form-group">
+            <button type="submit" name= "submit" class="btn ">Valider</button>
+        </div>
+
+        <div class="form-group">
+            <button type="submit" name= "logout" class="btn ">Deco</button>
+        </div> 
+    </form>
+</section>
+
+<section class= "formulaireUp">
+
+    <h2 class= "sous-titre" >
+        Update
+    </h2>
+
+    <form class= "formUp"  method= "post">
+
+            <div class= "form-group">
+                <input type= "text" name= "newlogin" placeholder= "newlogin" autocomplete= "off">
+            </div>
+
+            <div class= "form-group">
+                <input type= "password" name= "newpassword" placeholder= "newpassword" autocomplete= "off">
+            </div>
+
+            <div class= "form-group">
+                <input type= "text" name= "newemail" placeholder= "newemail" autocomplete= "off">
+            </div>
+
+            <div class= "form-group">
+                <input type= "text" name= "newfirstname" placeholder= "newfirstname" autocomplete= "off">
+            </div>
+
+            <div class= "form-group">
+                <input type= "text" name= "newlastname" placeholder= "newlastname" autocomplete= "off">
+            </div>
+
+            <div class="form-group">
+                <button type="submit" name= "update" class="btn ">Update</button>
+            </div> 
+    </form>
+</section>
